f60b3b6906c61524d0a3a80634626b3d5ae4953f
EranKT <eran@ktfgalaxy.com>
2018-01-29 09:25:28 +0100

    project uploaded

---
+++ NewNotes/FirstViewController.swift
@@ -0,0 +1,325 @@
+//
+//  FirstViewController.swift
+//  NewNotes
+//
+//  Created by EKT DIGIDESIGN on 11/2/17.
+//  Copyright © 2017 EKT DIGIDESIGN. All rights reserved.
+//
+
+
+
+import UIKit
+import GoogleMobileAds
+
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate, GADBannerViewDelegate {
+      
+    var _list1 = [String]()
+    var _list2 = [Array<Any>]()
+    var _pricesArrays = [Array<Any>]()
+
+    @IBOutlet weak var myInputField: UITextField!
+    @IBOutlet weak var _myTableView: UITableView!
+    @IBOutlet weak var navBar: UINavigationItem!
+    @IBOutlet weak var button: UINavigationItem!
+
+    @IBOutlet weak var _viewForBanner: UIView!
+    ////// VIEW DID LOAD
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        _myTableView.register(SecondTableCell.classForCoder(), forCellReuseIdentifier: "_myCell")
+
+        let request: GADRequest!
+        request =  GADRequest()
+        let requestTestList = ["acc8f37f622185236d1f09abd03f2b5d",
+                               "df9dfa5edaf80f86ba79f3ad4b8787d9",
+                               "9f7ea1062afad6bf37c91212c5e228c4159e26a6",
+                               "461383f79e6c870f0b8ff22d30d1368b",
+                               "b0438b8c5bcb180bc6e70a0590e9bf01"]
+        
+        request.testDevices = requestTestList
+        adBannerView.load(request)
+        
+        myInputField.delegate = self
+
+        let identity = "MyArchiveArrayFilePath"
+        let identity2 = "MyArchiveArray2FilePath"
+        let identity3 = "MyArchiveArray3FilePath"
+
+        if UserDefaults.standard.value(forKey: identity) != nil
+        {
+            _list1 = (UserDefaults.standard.value(forKey: identity) as! [String])
+            _list2 = (UserDefaults.standard.value(forKey: identity2) as! [Array])
+            _pricesArrays = (UserDefaults.standard.value(forKey: identity3) as! [Array])
+            _myTableView.reloadData()
+        }
+    }
+
+ ////////////////////////////////////// TABLE VIEW //////////////////////////////
+    //COUNT NUMBER OF ROWS IN TABLE
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+        
+    {
+        return (_list1.count)
+    }
+    
+    //FILL TABLE CELLS WITH EXISTING ITEMS
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+         let cell = _myTableView.dequeueReusableCell(withIdentifier: "_myCell", for: indexPath) as! SecondTableCell
+        cell.backgroundColor = UIColor.init(red: 0, green: 0.8, blue: 0.1, alpha: 0.3)
+        
+        cell.textLabel?.text = _list1[indexPath.row]
+        let priceListForCell = _pricesArrays[indexPath.row] as! [CGFloat]
+        let totalPrice = priceListForCell.reduce(0, +)
+        
+        if totalPrice != 0
+        {
+            cell.priceLabel.text = "\(totalPrice)"
+        }
+        else
+        {
+            cell.priceLabel.text = " "
+        }
+        return cell
+    }
+ // EDIT TABLE
+    public func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+
+    public func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+   
+        
+        //>> EDIT TEXT ACTION
+        let editTextAction = UITableViewRowAction(style: .normal, title: "Edit text", handler: { (action, indexPath) in
+            let alert = UIAlertController(title: "", message: "Set text", preferredStyle: .alert)
+            
+            alert.addTextField(configurationHandler: { (textField) in
+                self.myInputField.text = ""
+            })
+            alert.addAction(UIAlertAction(title: "Update", style: .default, handler: { (updateAction) in
+                
+                self._list1[indexPath.row] = alert.textFields!.first!.text!
+                self._myTableView.reloadRows(at: [indexPath], with: .fade)
+                
+                self.saveData()
+                
+            }))
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            self.present(alert, animated: false)
+        })
+        
+        let deleteAction = UITableViewRowAction(style: .default, title: "Delete", handler: { (action, indexPath) in
+            self._list1.remove(at: indexPath.row)
+            self._list2.remove(at: indexPath.row)
+            self._pricesArrays.remove(at: indexPath.row)
+            self.saveData()
+            tableView.reloadData()
+        })
+        editTextAction.backgroundColor = UIColor.magenta
+  //>> EDIT TEXT ACTION
+        //>> DELETE ACTION
+
+        
+        return [deleteAction, editTextAction]
+    }
+
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool
+     {
+        let val = true
+        return val
+    }
+
+     func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+
+        var item :String!
+       item = _list1[sourceIndexPath.row]
+        self._list1.remove(at: sourceIndexPath.row)
+        self._list1.insert(item, at: destinationIndexPath.row)
+        
+        //<< UPDATE SUB TABLE ARRAY ACCORDING TO NEW ORDER
+        
+        var item2 :Array<Any>!
+                item2 = _list2[sourceIndexPath.row]
+                _list2.remove(at: sourceIndexPath.row)
+                _list2.insert(item2, at: destinationIndexPath.row)
+       
+        var item3 :Array<Any>!
+                item3 = _pricesArrays[sourceIndexPath.row]
+                _pricesArrays.remove(at: sourceIndexPath.row)
+                _pricesArrays.insert(item3, at: destinationIndexPath.row)
+        self.saveData()
+        //>> UPDATE SUB TABLE ARRAY ACCORDING TO NEW ORDER
+    }
+
+     public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+      
+//<< SAVE INDEX TO USER DEFAULT
+        let rowNumberString:String = String(format: "%i", indexPath.row)
+        let defaults: UserDefaults = UserDefaults.standard
+        defaults.set(rowNumberString, forKey: "selectedIndex")
+        defaults.synchronize()
+//>> SAVE INDEX TO USER DEFAULT
+
+//   LOAD SECOND TABLE VIEW
+        //OLD WAY
+ /*       let secondViewController = self.storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        self.present(secondViewController, animated: true, completion: nil)
+   */
+        ////////////////////////////////<< REPLACE VIEW WITH TRANSITION //////////////////////////////////////
+
+        guard let window = UIApplication.shared.keyWindow else {
+            return
+        }
+        
+        guard let rootViewController = window.rootViewController else {
+            return
+        }
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "SecondViewController")
+        vc.view.frame = rootViewController.view.frame
+        vc.view.layoutIfNeeded()
+        
+        UIView.transition(with: window, duration: 0.3, options: .transitionFlipFromRight, animations:
+            {
+            window.rootViewController = vc
+        }, completion: { completed in
+            // maybe do something here
+        })
+        
+        ////////////////////////////////>> REPLACE VIEW WITH TRANSITION //////////////////////////////////////
+
+    }
+    
+     public func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath?
+    {
+        return indexPath
+    }
+ 
+    //EDIT BUTTON ACTION
+    @IBAction func editButton(_ sender: Any)
+    {
+        let leftItem: UIBarButtonItem
+        
+        let val = true
+        _myTableView.setEditing(!_myTableView.isEditing, animated:val)
+        
+        if (_myTableView.isEditing)
+        {
+            leftItem = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(FirstViewController.editButton(_:)))
+        }
+        else
+        {
+            leftItem = UIBarButtonItem(title: "Reorder", style: .plain, target: self, action: #selector(FirstViewController.editButton(_:)))
+        }
+        
+        navBar.rightBarButtonItem = leftItem;
+        
+        _myTableView.reloadData()
+        self.saveData()
+        print("LIST1:",_list1)
+        print("LIST2:",_list2)
+        print("LIST3:",_pricesArrays)
+    }
+
+    //ADD ITEM BUTTON ACTION
+    @IBAction func AddItemButton(_ sender: Any)
+    {
+        myInputField.resignFirstResponder()
+        
+        self.addNewItemToList()
+    }
+   
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool // called when 'return' key pressed. return NO to ignore.
+    {
+        textField.resignFirstResponder()
+       
+        self.addNewItemToList()
+        
+        return true
+    }
+
+    func addNewItemToList() {
+        if (myInputField.text != "")
+        {
+            _list1.append(myInputField.text!)
+            myInputField.text = ""
+            _myTableView.reloadData()
+            
+            // create new eampty sub array for field
+            let newListArray = [String]()
+            let newPriceArray = [Int]()
+            _list2.append(newListArray)
+            _pricesArrays.append(newPriceArray)
+            
+            self.saveData()
+        }
+    }
+    
+    //REFRESH TABLE
+    override func viewDidAppear(_ animated: Bool)
+    {
+        _myTableView.reloadData()
+    }
+    
+    func saveData()
+    {
+        let identity = "MyArchiveArrayFilePath"
+        let identity2 = "MyArchiveArray2FilePath"
+        let identity3 = "MyArchiveArray3FilePath"
+
+        UserDefaults.standard.setValue(_list1, forKey: identity)
+        UserDefaults.standard.setValue(_list2, forKey: identity2)
+        UserDefaults.standard.setValue(_pricesArrays, forKey: identity3)
+        UserDefaults.standard.synchronize()
+    }
+    
+    //ADS CODE
+    lazy var adBannerView: GADBannerView = {
+        let adBannerView = GADBannerView(adSize: kGADAdSizeSmartBannerPortrait)
+        adBannerView.adUnitID = "ca-app-pub-4450381878329799/3303179267"
+        adBannerView.delegate = self
+        adBannerView.rootViewController = self
+ 
+        return adBannerView
+    }()
+    
+    func adViewDidReceiveAd(_ bannerView: GADBannerView) {
+        print("Banner loaded successfully")
+
+        _viewForBanner.frame = CGRect.init(x: self.view.frame.width/2 - bannerView.frame.width/2,
+                                           y: self.view.frame.height - bannerView.frame.height,
+                                           width: bannerView.frame.width,
+                                           height: bannerView.frame.height)
+        _viewForBanner.addSubview(bannerView)
+   //     _myTableView.tableHeaderView?.frame = bannerView.frame
+   //     _myTableView.tableHeaderView = bannerView
+    }
+    
+    func adView(_ bannerView: GADBannerView, didFailToReceiveAdWithError error: GADRequestError) {
+        print("Fail to receive ads")
+        print(error)
+    }
+
+   
+    override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
+        //    print("ROTATED")
+        _viewForBanner.frame = CGRect.init(x: self.view.frame.width/2 - adBannerView.frame.width/2,
+                                           y: self.view.frame.height - adBannerView.frame.height,
+                                           width: adBannerView.frame.width,
+                                           height: adBannerView.frame.height)
+        _viewForBanner.addSubview(adBannerView)
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
---
+++ NewNotes/SecondViewController.swift
@@ -0,0 +1,351 @@
+//
+//  SecondViewController.swift
+//  NewNotes
+//
+//  Created by EKT DIGIDESIGN on 11/2/17.
+//  Copyright © 2017 EKT DIGIDESIGN. All rights reserved.
+//
+
+
+import UIKit
+import GoogleMobileAds
+
+class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate, GADBannerViewDelegate {
+ 
+ // this array keep list for each item in main menu
+    var _arrayOfSubLists = [Array<Any>]()
+    var _arrayOfpricesLists = [Array<Any>]()
+
+    var _currentViewList = [String]()
+    var _pricesArray = [CGFloat]()
+    var _indexForCurrentListInMainList = Int()
+    
+    @IBOutlet weak var _secondTableView: UITableView!
+    @IBOutlet weak var navBar: UINavigationItem!
+    @IBOutlet weak var _inputField: UITextField!
+    @IBOutlet weak var _priceLabel: UILabel!
+    
+    @IBOutlet weak var _viewForBanner: UIView!
+    ////// VIEW DID LOAD
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        _secondTableView.register(SecondTableCell.classForCoder(), forCellReuseIdentifier: "_MyCell2")
+        let request: GADRequest!
+        request =  GADRequest()
+        let requestTestList = ["acc8f37f622185236d1f09abd03f2b5d",
+                               "df9dfa5edaf80f86ba79f3ad4b8787d9",
+                               "9f7ea1062afad6bf37c91212c5e228c4159e26a6",
+                               "461383f79e6c870f0b8ff22d30d1368b",
+                               "b0438b8c5bcb180bc6e70a0590e9bf01"]
+        
+        request.testDevices = requestTestList
+        adBannerView.load(request)
+        //  adBannerView.load(GADRequest())
+        
+        _inputField.delegate = self
+        //self.textFieldShouldReturn(myInputField)
+        // myInputField.textField.delegate = self
+
+        let defaults: UserDefaults = UserDefaults.standard
+        let indexStr = defaults.value(forKey: "selectedIndex") as? String
+        _indexForCurrentListInMainList = Int(indexStr!)!
+        
+        let _listIdentity = "MyArchiveArray2FilePath"
+        let _pricesListIdentity = "MyArchiveArray3FilePath"
+        if UserDefaults.standard.value(forKey: _listIdentity) != nil
+        {
+            _arrayOfSubLists = (UserDefaults.standard.value(forKey: _listIdentity) as! [Array])
+            _arrayOfpricesLists = (UserDefaults.standard.value(forKey: _pricesListIdentity) as! [Array])
+            _currentViewList = _arrayOfSubLists[_indexForCurrentListInMainList] as! [String]
+            _pricesArray = _arrayOfpricesLists[_indexForCurrentListInMainList] as! [CGFloat]
+      
+            // add place holders for prices incase of app update
+            for _ in 0 ..< _currentViewList.count
+            {
+                if _pricesArray.count < _currentViewList.count
+                {
+                    _pricesArray.append(0)
+                    if _pricesArray.count == _currentViewList.count{break}
+
+                }
+            }
+        }
+        self.saveData()
+    }
+    
+/////////// TABLE VIEW
+    // BUG: when adding this method disapear
+    // public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+   //     return 150
+   // }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return (_currentViewList.count)
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = _secondTableView.dequeueReusableCell(withIdentifier: "_MyCell2", for: indexPath) as! SecondTableCell
+        cell.backgroundColor = UIColor.init(red: 0, green: 0.8, blue: 0.1, alpha: 0.3)
+
+        cell.textLabel?.text = _currentViewList[indexPath.row]
+        if _pricesArray[indexPath.row] != 0
+        {
+            cell.priceLabel.text = "\(_pricesArray[indexPath.row])"
+        }
+        else
+        {
+        cell.priceLabel.text = " "
+        }
+        return cell
+    }
+ /// ////////// EDIT ITEMS IN TABLE
+    public func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    public func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+  
+        //<< EDIT TEXT ACTION
+      let editTextAction = UITableViewRowAction(style: .normal, title: "Name", handler: { (action, indexPath) in
+            let alert = UIAlertController(title: "", message: "Set text", preferredStyle: .alert)
+            
+            alert.addTextField(configurationHandler: { (textField) in
+                self._inputField.text = ""
+            })
+            alert.addAction(UIAlertAction(title: "Update", style: .default, handler: { (updateAction) in
+
+                self._currentViewList[indexPath.row] = alert.textFields!.first!.text!
+                self._secondTableView.reloadRows(at: [indexPath], with: .fade)
+                
+                 self.saveData()
+                
+            }))
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            self.present(alert, animated: false)
+        })
+        editTextAction.backgroundColor = UIColor.magenta
+  //>> EDIT TEXT ACTION
+
+        //<< EDIT PRICE ACTION
+        let editPriceAction = UITableViewRowAction(style: .normal, title: "Price", handler: { (action, indexPath) in
+            let alert = UIAlertController(title: "", message: "Set price", preferredStyle: .alert)
+            
+            alert.addTextField(configurationHandler: { (textField) in
+             textField.keyboardType = UIKeyboardType.numberPad
+            })
+            alert.addAction(UIAlertAction(title: "Update", style: .default, handler: { (updateAction) in
+                let floatPrice = CGFloat((alert.textFields!.first!.text! as NSString).floatValue)
+
+                self._pricesArray[indexPath.row] = floatPrice
+                self._secondTableView.reloadRows(at: [indexPath], with: .fade)
+            
+            self.saveData()
+                
+            }))
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            self.present(alert, animated: false)
+        })
+        
+        editPriceAction.backgroundColor = UIColor.blue
+      //>> EDIT PRICE ACTION
+        //<< DELETE ACTION
+        let deleteAction = UITableViewRowAction(style: .default, title: "Delete", handler: { (action, indexPath) in
+            self._currentViewList.remove(at: indexPath.row)
+            self._pricesArray.remove(at: indexPath.row)
+            self.saveData()
+        })
+        
+        let resetAction = UITableViewRowAction(style: .normal, title: "Reset Prices", handler: { (action, indexPath) in
+        for i in 0 ..< self._pricesArray.count
+        {
+            self._pricesArray[i] = 0.0
+            }
+            self.saveData()
+        })
+        resetAction.backgroundColor = UIColor.brown
+//>> DELETE ACTION
+        return [deleteAction,editTextAction, editPriceAction, resetAction]
+    }
+ 
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool
+    { 
+        let val = true
+        return val
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        var item :String!
+        item = _currentViewList[sourceIndexPath.row]
+        self._currentViewList.remove(at: sourceIndexPath.row)
+        self._currentViewList.insert(item, at: destinationIndexPath.row)
+        var price :CGFloat!
+        price = _pricesArray[sourceIndexPath.row]
+        self._pricesArray.remove(at: sourceIndexPath.row)
+        self._pricesArray.insert(price, at: destinationIndexPath.row)
+        
+        self.saveData()
+    }
+
+    
+    //REFRESH TABLE
+    override func viewDidAppear(_ animated: Bool)
+    {
+        _secondTableView.reloadData()
+    }
+///////////// BUTTONS //////////////////
+    //// EDIT BUTTON
+    @IBAction func EditButton(_ sender: Any)
+    {
+        let rightItem: UIBarButtonItem
+        
+        let val = true
+        _secondTableView.setEditing(!_secondTableView.isEditing, animated:val)
+        
+        if (_secondTableView.isEditing)
+        {
+            rightItem = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(SecondViewController.EditButton(_:)))
+        }
+        else
+        {
+            rightItem = UIBarButtonItem(title: "Reorder", style: .plain, target: self, action: #selector(SecondViewController.EditButton(_:)))
+        }
+        
+        navBar.rightBarButtonItem = rightItem;
+
+        self.saveData()
+    }
+    //// ADD BUTTON
+    @IBAction func addButton(_ sender: Any)
+    {
+        self.addNewItemToList()
+        _inputField.resignFirstResponder()
+    }
+    
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool // called when 'return' key pressed. return NO to ignore.
+    {
+        self.addNewItemToList()
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func addNewItemToList() {
+        if (_inputField.text != "")
+        {
+            _currentViewList.append(_inputField.text!)
+            _pricesArray.append(0)
+            _inputField.text = ""
+            
+            self.saveData()
+        }
+    }
+    
+    func setTitle() {
+        
+        let identityOriginalList = "MyArchiveArrayFilePath"
+        var originalList = (UserDefaults.standard.value(forKey: identityOriginalList) as! [String])
+        let totalPrice = _pricesArray.reduce(0, +)
+        let strPrice: String
+        let title: String
+        
+        if totalPrice == 0{
+            strPrice = ""
+            title = originalList[self._indexForCurrentListInMainList]
+  }
+        else
+        {
+            strPrice = String(describing: totalPrice)
+            title = strPrice
+        }
+        
+        self.navBar.title = title
+    }
+    
+    func saveData()
+    {
+        self.setTitle()
+        _secondTableView.reloadData()
+
+        let _listIdentity = "MyArchiveArray2FilePath"
+        let _pricesListIdentity = "MyArchiveArray3FilePath"
+
+        _arrayOfSubLists[_indexForCurrentListInMainList] = _currentViewList
+        _arrayOfpricesLists[_indexForCurrentListInMainList] = _pricesArray
+        UserDefaults.standard.setValue(_arrayOfSubLists, forKey: _listIdentity)
+        UserDefaults.standard.setValue(_arrayOfpricesLists, forKey: _pricesListIdentity)
+    }
+    
+  
+    @IBAction func BackButton(_ sender: Any)
+    {
+        guard let window = UIApplication.shared.keyWindow else {
+            return
+        }
+        
+        guard let rootViewController = window.rootViewController else {
+            return
+        }
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "FirstViewController")
+        vc.view.frame = rootViewController.view.frame
+        vc.view.layoutIfNeeded()
+        
+        UIView.transition(with: window, duration: 0.3, options: .transitionFlipFromLeft, animations:
+            {
+                window.rootViewController = vc
+        }, completion: { completed in
+        })
+        
+        ////////////////////////////////>> REPLACE VIEW WITH TRANSITION //////////////////////////////////////
+
+        
+    }
+    
+    
+    
+    
+    //ADS CODE
+    lazy var adBannerView: GADBannerView = {
+        let adBannerView = GADBannerView(adSize: kGADAdSizeSmartBannerPortrait)
+        adBannerView.adUnitID = "ca-app-pub-4450381878329799/3303179267"
+        adBannerView.delegate = self
+        adBannerView.rootViewController = self
+        
+        return adBannerView
+    }()
+    
+    func adViewDidReceiveAd(_ bannerView: GADBannerView) {
+        print("Banner loaded successfully")
+
+        _viewForBanner.frame = CGRect.init(x: self.view.frame.width/2 - bannerView.frame.width/2,
+                                           y: self.view.frame.height - bannerView.frame.height,
+                                           width: bannerView.frame.width,
+                                           height: bannerView.frame.height)
+        _viewForBanner.addSubview(bannerView)
+
+        //  _secondTableView.tableHeaderView?.frame = bannerView.frame
+      //  _secondTableView.tableHeaderView = bannerView
+        
+    }
+    
+    override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
+        //    print("ROTATED")
+        _viewForBanner.frame = CGRect.init(x: self.view.frame.width/2 - adBannerView.frame.width/2,
+                                           y: self.view.frame.height - adBannerView.frame.height,
+                                           width: adBannerView.frame.width,
+                                           height: adBannerView.frame.height)
+        _viewForBanner.addSubview(adBannerView)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
